8ffbe844334b2aec74445763a4fbed60875c6e5e
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-05-05 23:45:45 +0300

    Update rassilca.py

--- rassilca.py
+++ rassilca.py
@@ -1,18 +1,18 @@
 joinedFile = open("/joined.txt", "r")
-joinedUser = set()
+joinedUsers = set()
 for line in joinedFile:
   joinedUsers.add(line.strip())
 joinedFile.close()
 
-@bot.message_hendler(commands=['start'])
-def startJoin(massage):
-  if not str(massage.chat.id) in joinedUsers:
+@bot.message_handler(commands=['start'])
+def startJoin(message):
+  if not str(message.chat.id) in joinedUsers:
     joinedFile=open("/joined.txt", "a")
-    joinedFile.write(str(massage.chat.id) + "\n")
-    joinedUsers.add(massage.chat.id)
+    joinedFile.write(str(message.chat.id) + "\n")
+    joinedUsers.add(message.chat.id)
     
-@bot.message_hendler(commands=['special'])
-def mess(massage):
+@bot.message_handler(commands=['special'])
+def mess(message):
   for user in joinedUsers:
-    bot.send_massage(user, massage.text[massage.text.find(' '):])
+    bot.send_message(user, message.text[message.text.find(' '):])
                      
