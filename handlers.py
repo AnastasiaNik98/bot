90423fab9123a3cd6622460cf475cfb3c3d8b4df
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-05-06 00:28:24 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -9,14 +9,12 @@ for line in joinedFile:
   joinedUsers.add(line.strip())
 joinedFile.close()
 
-@bot.message_handler(commands=['start'])
 def startJoin(message):
   if not str(message.chat.id) in joinedUsers:
     joinedFile=open("/joined.txt", "a")
     joinedFile.write(str(message.chat.id) + "\n")
     joinedUsers.add(message.chat.id)
     
-@bot.message_handler(commands=['special'])
 def mess(message):
   for user in joinedUsers:
     update.send_message(user, message.text[message.text.find(' '):])
