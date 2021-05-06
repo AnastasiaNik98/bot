40d1dd3cd5102153c33bbd64d2e23b94b54ff05c
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-05-06 21:06:37 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -12,10 +12,11 @@ def start(update, context):
     context.bot.send_message(
         chat_id=TELEGRAM_SUPPORT_CHAT_ID,
         text=f"""
+        
 📞 Connected {user_info}.
         """,
     )
-    
+      
 
     
 def about(update, context):
