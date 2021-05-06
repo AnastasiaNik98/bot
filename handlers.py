724d3cc59266c96f300b9c13453bf452523a6f07
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-05-06 23:51:02 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -3,7 +3,7 @@ import telebot
 from telegram.ext import CommandHandler, MessageHandler, Filters
 from settings import WELCOME_MESSAGE, TELEGRAM_SUPPORT_CHAT_ID
 from telebot import types
-
+bot = telebot.TeleBot(config.token)
 def info(update, context):
     
     update.message.reply_text(WELCOME_MESSAGE)
@@ -22,31 +22,31 @@ def info(update, context):
 
 @bot.message_handler(commands=["start"])
 def start(m):
-    msg = update.send_message(m.chat.id, "Вас приветствует Бот")
+    msg = bot.send_message(m.chat.id, "Вас приветствует Бот")
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*[types.KeyboardButton(name) for name in ['О компании', 'Прайс-лист']])
     keyboard.add(*[types.KeyboardButton(name) for name in ['Акции', 'Контакты']])
-    update.send_message(m.chat.id, 'Выберите в меню что вам интересно!',
+    bot.send_message(m.chat.id, 'Выберите в меню что вам интересно!',
         reply_markup=keyboard)
-    update.register_next_step_handler(msg, name)
+    bot.register_next_step_handler(msg, name)
 
 def name(m):
     if m.text == 'О компании':
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(*[types.KeyboardButton(advert) for advert in ['Сертификаты']])
         keyboard.add(*[types.KeyboardButton(advert) for advert in ['В начало']])
-        update.send_message(m.chat.id, 'инфа о компании',
+        bot.send_message(m.chat.id, 'инфа о компании',
             reply_markup=keyboard)
     elif m.text == 'Прайс-лист':
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(*[types.KeyboardButton(advert) for advert in ['Общий', 'Одиночный']])
         keyboard.add(*[types.KeyboardButton(advert) for advert in ['В начало']])
-        update.send_message(m.chat.id, 'Выберите прайс который нужен.',
+        bot.send_message(m.chat.id, 'Выберите прайс который нужен.',
             reply_markup=keyboard)
     elif m.text == 'Акции':
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(*[types.KeyboardButton(advert) for advert in ['В начало']])
-        update.send_message(m.chat.id, 'Сожалею, но в данный момент акций нет(',
+        bot.send_message(m.chat.id, 'Сожалею, но в данный момент акций нет(',
             reply_markup=keyboard)
     
 def about(update, context):
