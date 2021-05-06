e9471a96cb708b18cac509443dff6f07d55af493
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-05-06 12:46:29 +0300

    Create rassilca.py

---
+++ rassilca.py
@@ -0,0 +1,18 @@
+import os
+import telebot
+joinedFile = open("https://github.com/AnastasiaNik98/bot/blob/main/joined.txt", "r")
+joinedUsers = set()
+for line in joinedFile:
+  joinedUsers.add(line.strip())
+  joinedFile.close()
+  
+@bot.message_handler(commands=['start'])
+  def startJoin(message):
+    if not str(message.chat.id) in joinedUsers:
+      joinedFile = open("https://github.com/AnastasiaNik98/bot/blob/main/joined.txt", "a")
+      joinedFile.write(str(message.chat.id)+"\n")
+      joinedUsers.add(message.chat.id)
+@bot.message_handler(commands=['special'])
+def mess(message):
+  for user in joinedUsers:
+    bot.send_message(user, message.text[message.text.find(' '):])
