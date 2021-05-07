ed8054d0f526e2192e0f4986b553021433a0829a
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-05-07 19:46:10 +0300

    Update rassilca.py

--- rassilca.py
+++ rassilca.py
@@ -6,13 +6,13 @@ for line in joinedFile:
   joinedUsers.add(line.strip())
   joinedFile.close()
   
-@bot.message_handler(commands=['start'])
+
   def startJoin(message):
     if not str(message.chat.id) in joinedUsers:
       joinedFile = open("/joined.txt", "a")
-      joinedFile.write(str(chat_id)+"\n")
+      joinedFile.write(str(message.chat.id)+"\n")
       joinedUsers.add(message.chat.id)
-@bot.message_handler(commands=['info'])
+
 def mess(message):
   for user in joinedUsers:
-    update.send_message(user, message.text[message.text.find(' '):])
+    bot.send_message(user, message.text[message.text.find(' '):])
