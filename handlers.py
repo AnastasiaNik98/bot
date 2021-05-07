b7dace22e9c453d83fc4b6a52119e6554eac24e0
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-05-07 14:54:02 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -31,13 +31,7 @@ def help(update, context):
 
 
 def forward_to_chat(update, context):
-    """{ 
-        'message_id': 5, 
-        'date': 1605106546, 
-        'chat': {'id': 49820636, 'type': 'private', 'username': 'danokhlopkov', 'first_name': 'Daniil', 'last_name': 'Okhlopkov'}, 
-        'text': 'TEST QOO', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-        'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
-    }"""
+   
     update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
 
 
