ada1d96a08e8fe5c4f5bc1e5266eed6b5e32c6ff
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-05-07 19:22:25 +0300

    Update rassilca.py

--- rassilca.py
+++ rassilca.py
@@ -1,17 +1,20 @@
 
+import telebot
+from telegram.ext import Updater
+updater = Updater(TELEGRAM_TOKEN)
 joinedFile = open("/joined.txt", "r")
 joinedUsers = set()
 for line in joinedFile:
   joinedUsers.add(line.strip())
   joinedFile.close()
   
-
+@bot.message_handler(commands=['start'])
   def startJoin(message):
-    if not str(chat_id) in joinedUsers:
+    if not str(message.chat.id) in joinedUsers:
       joinedFile = open("/joined.txt", "a")
       joinedFile.write(str(chat_id)+"\n")
-      joinedUsers.add(chat_id)
-
+      joinedUsers.add(message.chat.id)
+@bot.message_handler(commands=['info'])
 def mess(message):
   for user in joinedUsers:
     update.send_message(user, message.text[message.text.find(' '):])
