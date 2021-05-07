2a7d7a6ec6f0e8eaa61a045f65584c22454fec62
AnastasiaNik98 <83593083+AnastasiaNik98@users.noreply.github.com>
2021-05-07 20:33:20 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -23,7 +23,7 @@ joinedFile = open('joined.txt', 'r')
 joinedUsers = set()
 for line in joinedFile:
 joinedUsers.add(line.strip())
-joinedFile.write(str(chat_id)+"\n")
+joinedFile.write("Ghbdtn")
 joinedFile.close()
 
 
